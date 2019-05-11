d1eb9844733ffaa6aae400d5aeb7148ff50f4841
przewnic <przewnic@mion.elka.pw.edu.pl>
2019-05-11 22:38:57 +0200

    Fixed srv and service

--- src/testbot_lab2/scripts/jint_script.py
+++ src/testbot_lab2/scripts/jint_script.py
@@ -1,25 +1,26 @@
 #! /usr/bin/python
 
-import  numpy as np
+import numpy as np
 import rospy
 import os
 import math
 from tf.transformations import *
 from sensor_msgs.msg import JointState
 from geometry_msgs.msg import PoseStamped
+from testbot_lab2.srv import *
 
 
 def to_joint_state(px,py,pz):
+	global pub
 	js = JointState()
 	js.name.append('joint1')
 	js.name.append('joint2')
 	js.name.append('joint3')
-	js.position.resize(3)
 	js.header.stamp = rospy.Time.now()
-	js.position[0] = px
-	js.position[1] = py
-	js.position[2] = pz
-	joint.publish(js)
+	js.position.append(pz)
+	js.position.append(py)
+	js.position.append(px)
+	pub.publish(js)
 
 def inter_lin(time,target_pos,prev_pos):
 	next_pos = prev_pos + ((target_pos-prev_pos)/(time*10.0))
@@ -29,12 +30,15 @@ def inter_lin(time,target_pos,prev_pos):
 
 def interpolation(information):
 #todo check data
+	global prev_x
+	global prev_y
+	global prev_z
 	if(information.x > 100 or information.y >100 or information.z >100):
-		information.status = 'Wrong values'
-		return
+		#information.status = 'Wrong values'
+		return 'Wrong values'
 	if(information.time <= 0):
-		information.status = 'Wrong time value'
-		return
+		#information.status = 'Wrong time value'
+		return 'Wrong time value'
 
 	if(information.type == 'linear'):
  
@@ -49,14 +53,14 @@ def interpolation(information):
 		prev_x = px
 		prev_y = py
 		prev_z = pz
-		information.status = 'ok'
-		return
+		#information.status = 'ok'
+		return 'ok-done lineary'
 
 	elif(infrmation.type == 'poly'):
-		return
+		return 'ok'
 	else:
-		information.status = 'wrong type of interpolation'
-		return
+		#information.status = 'wrong type of interpolation'
+		return 'wrong type of interpolation'
 
 
 
@@ -65,12 +69,13 @@ if __name__ == '__main__':
 	prev_y=0
 	prev_z=0
 
-	rospy.init_node('jint', anonymous=True)
+	rospy.init_node('my_jint', anonymous=True)
 
 	print os.path.dirname(os.path.realpath(__file__))
   
 	pub = rospy.Publisher('jointState', JointState, queue_size = 10)
-	rospy.wait_for_service('Jint')
-	input_data = rospy.Service('jint', Jint , interpolation)
+	#rospy.wait_for_service('jint')
+	input_data = rospy.Service('jint', myjint, interpolation)
+	rospy.loginfo( 'Ready -------------service jint---------------')
 	rospy.spin()
 
